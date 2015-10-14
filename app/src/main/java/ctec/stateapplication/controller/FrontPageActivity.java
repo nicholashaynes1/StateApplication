48161269ec05b40df397a21cec8b15fda9ceb4db
nicholashaynes1 <nicholashaynes1@gmail.com>
2015-10-14 12:16:36 -0600

    Created a new application.

---
+++ app/src/main/java/ctec/stateapplication/controller/FrontPageActivity.java
@@ -0,0 +1,62 @@
+package ctec.stateapplication.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.CheckBox;
+import android.view.View;
+import android.content.Intent;
+
+
+
+
+
+public class FrontPageActivity extends Activity
+{
+    private Button nextScreenButton;
+    private EditText nameText;
+    private EditText ageText;
+    private CheckBox isTiredBox;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front_page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_front_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupListeners()
+    {
+
+    }
+
+
+}
